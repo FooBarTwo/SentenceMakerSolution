b6efaf347fbd2c4b2a5cbf354e9d6e074d7ad374
Lester Findlay <Lester@dm.co.za>
2022-04-11 13:57:06 +0200

    Adds REST Api and configuration changes.

---
+++ SentenceManagement.Api/Models/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WordManagement.Models;
+
+namespace SentenceManagement.Api.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base (options)
+        {
+        }
+
+        public DbSet<Sentence> Sentences { get; set; }
+        public DbSet<Word> Words { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed Sentences Table
+            modelBuilder.Entity<Sentence>().HasData(new Sentence { SentenceId = 1, Content = "Hello World!" });
+            modelBuilder.Entity<Sentence>().HasData(new Sentence { SentenceId = 2, Content = "Goodbye cruel World." });
+            modelBuilder.Entity<Sentence>().HasData(new Sentence { SentenceId = 3, Content = "Whatever you say is only you're opinion." });
+            modelBuilder.Entity<Sentence>().HasData(new Sentence { SentenceId = 4, Content = "When the blueberry sings and the mocking bird hums." });
+            modelBuilder.Entity<Sentence>().HasData(new Sentence { SentenceId = 5, Content = "When comes and the sun chaseth the darkness away." });
+
+            // Seed Word Table
+            //NOUNS: Box,Brother,Business,Car,Case,Change,Child,
+            //VERBS: Roar,Ride,Roast,Run,Say,Sing,Sit
+            modelBuilder.Entity<Word>().HasData(new Word { WordId = 1, TypeOfWord = WordType.Noun, Descrip = "Box" });
+            modelBuilder.Entity<Word>().HasData(new Word { WordId = 2, TypeOfWord = WordType.Noun, Descrip = "Car" });
+            modelBuilder.Entity<Word>().HasData(new Word { WordId = 3, TypeOfWord = WordType.Noun, Descrip = "Case" });
+            modelBuilder.Entity<Word>().HasData(new Word { WordId = 4, TypeOfWord = WordType.Verb, Descrip = "Ride" });
+            modelBuilder.Entity<Word>().HasData(new Word { WordId = 5, TypeOfWord = WordType.Verb, Descrip = "Sit" });
+            modelBuilder.Entity<Word>().HasData(new Word { WordId = 6, TypeOfWord = WordType.Verb, Descrip = "Sing" });
+        }
+    }
+}
--- SentenceManagement.Api/Startup.cs
+++ SentenceManagement.Api/Startup.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SentenceManagement.Api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SentenceManagement.Api
 {
@@ -25,6 +27,7 @@ namespace SentenceManagement.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
             services.AddControllers();
         }
 
